d5c57edd7e1c05c967ae76103f8381f46ed2e20d
Mohammad Rashid <dopplesoldner@gmail.com>
2013-06-03 21:29:00 +0100

    Finish costfunction.m

--- logistic_regression/mlclass-ex2/costFunction.m
+++ logistic_regression/mlclass-ex2/costFunction.m
@@ -15,15 +15,5 @@ h_theta_x = sigmoid(X * theta); %hypothesis
 J = sum(-y .* log(h_theta_x) - (1 - y) .* log(1 - h_theta_x)) * (1/m);
 
 grad = (X' * (h_theta_x - y)) * (1/ m);
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-% =============================================================
 
 end
